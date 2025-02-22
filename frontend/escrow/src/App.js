9ec22c2de93f56146dea727b08ee1472cf805108
dvenkat <dhruv.venkat.2024@gmail.com>
2025-02-22 10:12:38 -0500

    basic react works

--- frontend/escrow/src/App.js
+++ frontend/escrow/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+//import { useQueryCall, useUpdateCall } from '@ic-reactor/react';
 import './App.css';
+// import motokoLogo from './assets/motoko_moving.png';
+// import motokoShadowLogo from './assets/motoko_shadow.png';
+// import reactLogo from './assets/react.svg';
+// import viteLogo from './assets/vite.svg';
 
 function App() {
+//   const { data: count, refetch } = useQueryCall({
+//     functionName: 'get',
+//   });
+
+//   const { call: increment, loading } = useUpdateCall({
+//     functionName: 'inc',
+//     onSuccess: refetch,
+//   });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      {/* <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo vite" alt="Vite logo" />
         </a>
-      </header>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a
+          href="https://internetcomputer.org/docs/current/developer-docs/build/cdks/motoko-dfinity/motoko/"
+          target="_blank"
+        >
+          <span className="logo-stack">
+            <img
+              src={motokoShadowLogo}
+              className="logo motoko-shadow"
+              alt="Motoko logo"
+            />
+            <img src={motokoLogo} className="logo motoko" alt="Motoko logo" />
+          </span>
+        </a>
+      </div> */}
+      <h1>Vite + React + Motoko</h1>
+      <div className="card">
+        <button > 
+            {/* onClick={increment} disabled={loading} */}
+          count is {1}
+        </button>
+        <p>
+          Edit <code>backend/Backend.mo</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite, React, and Motoko logos to learn more
+      </p>
     </div>
   );
 }
